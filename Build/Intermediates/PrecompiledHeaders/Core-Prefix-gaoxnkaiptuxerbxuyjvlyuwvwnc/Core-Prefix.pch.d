6e8342e8a03c2fd6c571d39e38d46da8317dafa0
王志跃 <zywang@isheely.com>
2013-11-04 14:35:55 +0800

    core

---
+++ Build/Intermediates/PrecompiledHeaders/Core-Prefix-gaoxnkaiptuxerbxuyjvlyuwvwnc/Core-Prefix.pch.d
@@ -0,0 +1,15 @@
+dependencies: /Users/wangzhiyue/Documents/ios/core/Core/Core-Prefix.pch \
+  /Users/wangzhiyue/Documents/ios/core/Core/Core.h \
+  /Users/wangzhiyue/Documents/ios/core/Core/extra/NVSkin.h \
+  /Users/wangzhiyue/Documents/ios/core/Core/net/SHPostTaskM.h \
+  /Users/wangzhiyue/Documents/ios/core/Core/net/SHTask.h \
+  /Users/wangzhiyue/Documents/ios/core/Core/obj/SHObject.h \
+  /Users/wangzhiyue/Documents/ios/core/Core/extra/Respinfo.h \
+  /Users/wangzhiyue/Documents/ios/core/Core/net/SHPostTask.h \
+  /Users/wangzhiyue/Documents/ios/core/Core/extra/Entironment.h \
+  /Users/wangzhiyue/Documents/ios/core/Core/update/SHVersion.h \
+  /Users/wangzhiyue/Documents/ios/core/Core/extra/SHIIOSVersion.h \
+  /Users/wangzhiyue/Documents/ios/core/Core/extra/Base64.h \
+  /Users/wangzhiyue/Documents/ios/core/Core/crash/SHCrashManager.h \
+  /Users/wangzhiyue/Documents/ios/core/Core/update/SHConfigManager.h \
+  /Users/wangzhiyue/Documents/ios/core/Core/obj/SHFuck.h
---
+++ Core/tool/SHTool.m
@@ -0,0 +1,25 @@
+//
+//  SHTool.m
+//  Core
+//
+//  Created by sheely on 13-11-1.
+//  Copyright (c) 2013年 zywang. All rights reserved.
+//
+
+#import "SHTool.h"
+
+@implementation SHTool
+
++ (BOOL)isJailbroken {
+    BOOL jailbroken = NO;
+    NSString *cydiaPath = @"/Applications/Cydia.app";
+    NSString *aptPath = @"/private/var/lib/apt/";
+    if ([[NSFileManager defaultManager] fileExistsAtPath:cydiaPath]) {
+        jailbroken = YES;
+    }
+    if ([[NSFileManager defaultManager] fileExistsAtPath:aptPath]) {
+        jailbroken = YES;
+    }
+    return jailbroken;
+}
+@end

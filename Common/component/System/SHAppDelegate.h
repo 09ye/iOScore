97c231e27accca57e8f8ef25b634e358f8c20ccf
WSheely <wsheely@WSheelydeiMac.local>
2014-04-09 15:22:22 +0800

    common

--- Common/component/System/SHAppDelegate.h
+++ Common/component/System/SHAppDelegate.h
@@ -12,6 +12,6 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
-@property (strong, nonatomic) ViewController *viewController;
+@property (strong, nonatomic) UIViewController *viewController;
 
 @end
